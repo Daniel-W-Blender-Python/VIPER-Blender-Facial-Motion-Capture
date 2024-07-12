66f42f3fd6624500bc50dee1f70b7f8cb8f56885
Daniel-W-Blender-Python <142774885+Daniel-W-Blender-Python@users.noreply.github.com>
2024-07-12 10:58:28 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,9 +22,10 @@ def install_dependencies():
     python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
     py_lib = os.path.join(sys.prefix, 'lib', 'site-packages','pip')
     subprocess.check_call([sys.executable, "-m", "ensurepip"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "mediapipe"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jaxlib"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jax"])
 
 def draw_file_opener(self, context):
     layout = self.layout
