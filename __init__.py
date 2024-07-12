4764d906479e02789d8a9cc79ac5039645550e89
Daniel-W-Blender-Python <142774885+Daniel-W-Blender-Python@users.noreply.github.com>
2024-07-12 10:14:20 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,6 +15,16 @@ from bpy_extras.io_utils import ImportHelper
 import bpy_extras
 import rigify
 from .run_face import run_face
+import subprocess
+import sys
+import os
+
+def install_dependencies():
+    python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+    py_lib = os.path.join(sys.prefix, 'lib', 'site-packages','pip')
+    subprocess.check_call([sys.executable, "-m", "ensurepip"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "mediapipe"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
 
 def draw_file_opener(self, context):
     layout = self.layout
@@ -220,3 +230,4 @@ def unregister():
 
 if __name__ == "__main__":
     register()    
+    install_dependencies()
