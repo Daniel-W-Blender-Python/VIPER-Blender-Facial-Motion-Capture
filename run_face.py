c96a20fa37f8367440bda782c1d7a157492ea68c
Daniel-W-Blender-Python <142774885+Daniel-W-Blender-Python@users.noreply.github.com>
2024-07-11 22:55:28 -0700

    Update run_face.py

--- run_face.py
+++ run_face.py
@@ -6,7 +6,7 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import mediapipe as mp
-from mediapipe.tasks import python
+#from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 def run_face(file_path):
